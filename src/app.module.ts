d2876408b3b390e3b6b0afa962f8c29fcaed0092
jorgeortizc06 <jorgeortizc06@gmail.com>
2022-06-16 13:19:30 -0500

    Configure Multer to permit the uploading and streaming of videos

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,22 @@ import { JwtModule } from '@nestjs/jwt';
 import { secret } from './utils/constants';
 import { join } from 'path/posix';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './public',
+        filename: (req, file, cb) => {
+          const ext = file.mimetype.split('/')[1];
+          cb(null, `${uuidv4()}-${Date.now()}.${ext}`);
+        },
+      }),
+    }),
     JwtModule.register({ secret, signOptions: { expiresIn: '2h' } }),
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
   ],
